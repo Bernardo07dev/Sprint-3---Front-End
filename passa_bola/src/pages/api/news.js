cf6f405f63fe7695c423f169047370adad83df0b
Bruna Sadi Duarte <101605299+brunasadi@users.noreply.github.com>
2025-09-15 19:48:53 -0300

    Update news.js

--- passa_bola/src/pages/api/news.js
+++ passa_bola/src/pages/api/news.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-  const API_KEY = process.env.API_KEY; 
+  const API_KEY = process.env.API_KEY;
 
   if (!API_KEY) {
-    console.error("Erro: A chave da API não está definida nas variáveis de ambiente do Vercel.");
     return res.status(500).json({ error: "Chave da API não configurada." });
   }
 
@@ -15,10 +14,9 @@ export default async function handler(req, res) {
     if (response.ok) {
       res.status(200).json(data);
     } else {
-      res.status(response.status).json({ error: data.message || "Erro ao buscar notícias da API externa." });
+      res.status(response.status).json({ error: data.message || "Erro ao buscar notícias." });
     }
   } catch (err) {
-    console.error("Erro ao buscar notícias:", err);
-    res.status(500).json({ error: "Erro interno do servidor ao processar a requisição." });
+    res.status(500).json({ error: "Erro interno do servidor." });
   }
 }
